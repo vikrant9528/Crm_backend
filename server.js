0c4e86c102862009c621e99b90c861a68c4e508d
vikrant9528 <vikrantchaudhary258@gmail.com>
2025-11-06 10:52:02 +0530

    some changes

--- server.js
+++ server.js
@@ -19,5 +19,8 @@ app.use("/followups",auth, followupRoutes);
 // Protected routes (require JWT)
 app.use("/leads", auth, leadRoutes);
 const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.DATABASE_URL);
-app.listen(PORT, () => console.log("Server running"));
+app.listen(PORT, () => console.log(`Server running${PORT}`));
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB connection failed:", err));
